5f252bf40ffb8dd93cfd39ffb09e0f77ad3b9a62
Maxence <maxencemalbois@gmail.com>
2022-09-04 18:28:08 +0200

    Update alter_saturation.rb

--- alter_saturation.rb
+++ alter_saturation.rb
@@ -1,5 +1,3 @@
-# require 'vips'
-
 # Here we are basically switching our image to LCH color space
 # each pixel now has a value from 0 to 255 for Luminance,Chroma Hue
 # we are then using the Linear method from http://libvips.github.io/ruby-vips/Vips/Image.html#linear-instance_method 
@@ -15,6 +13,8 @@
 # Also you can see that our saturation multiplicator is added the second value of our array
 # This is because in LCH colorspace, it is the first byte that corresponds to the luminance
 # If we had added x to all values of the array, then Luminance (brightness) and Hue would have been altered too
+
+require 'vips'
 
 x = 1.2
 
